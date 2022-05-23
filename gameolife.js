95e1de312175ccca3d460d7a2035161d4f0dca45
Eduardo <silva.eduardo@gmail.com>
2022-05-23 17:41:40 +0100

    rr

--- gameolife.js
+++ gameolife.js
@@ -35,8 +35,8 @@ function init() {
     setRules(rules);
     bugsImg.src = imgFileName;
     setSpriteMap(sprMap, bugsImg, 4, 4, gridSize, gridSize);
-    rows = Math.floor(window.innerWidth / gridSize);
-    cols = Math.floor(window.innerHeight / gridSize);
+    cols = Math.floor(window.innerWidth / gridSize);
+    rows = Math.floor(window.innerHeight / gridSize);
     elem = Array.matrix(cols, rows, -1);
     fillMatrix(elem, powers, densit);
 }
@@ -95,6 +95,25 @@ Array.copy = function (arrFrom) {
     return arr;
 };
 
+Array.resize = function (origin_arr, to_cols, to_rows, initial) {
+    var arr = Array.matrix(to_cols, to_rows, initial);
+    for (var r = 0; r < to_rows; ++r) {
+        if (origin_arr[r]) {
+            for (var c = 0; c < to_cols; ++c) {
+                arr[r][c] = origin_arr[r][c] ? origin_arr[r][c] : initial;
+            }
+        }
+        else {
+            var new_row = [];
+            for (var c = 0; c < to_cols; c++) {
+                new_row[c] = initial;
+            }
+            arr[r] = new_row;
+        }
+    }
+    return arr;
+};
+
 function addEvents() {
     window.addEventListener("mousemove", (event) => {
         mouse.x = event.x;
@@ -110,12 +129,19 @@ function resizeAll() {
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
     // Todo: implement - resize the matrix, variables, etc
+    let prev_rows = elem.length;
+    let prev_cols = elem[0].length;
+    cols = Math.floor(window.innerWidth / gridSize);
+    rows = Math.floor(window.innerHeight / gridSize);
+    elem = Array.resize(elem, cols, rows, -1);
+
 }
 
 function fillMatrix(matrix, powers, density) {
     var rows = matrix.length;
     var cols = matrix[0].length;
     var powerkeys = Object.keys(powers);
+    var counter = {}
     for (var r = 0; r < rows; r += 1) {
         for (var c = 0; c < cols; c += 1) {
             if (c == 0 || c == cols - 1 || r == 0 || r == rows - 1) {
@@ -123,11 +149,13 @@ function fillMatrix(matrix, powers, density) {
             } else {
                 if (Math.random() < density) {
                     var randChoice = Math.floor(Math.random() * powerkeys.length);
+                    counter[randChoice] = counter[randChoice] ? (counter[randChoice] + 1) : 1;
                     matrix[r][c] = powerkeys[randChoice];
                 } else matrix[r][c] = -1;
             }
         }
     }
+    console.log(counter);
 }
 
 function interact(matrix, powers, rules) {
@@ -177,25 +205,21 @@ function judge(cell, powers, rules) {
             }
         }
     }
-    try {
-        var bugKeys = Object.keys(bugs);
-        for (let kb = 0; kb < bugKeys.length; kb += 1) {
-            let bug = bugKeys[kb];
-            let points = 0;
-            for (var kob = 0; kob < bugKeys.length; kob += 1) {
-                var oBug = bugKeys[kob];
-                if (oBug in powers[bug]["s"]) {
-                    points -= bugs[oBug]["c"];
-                } else {
-                    if (oBug in powers[bug]["w"] || oBug === bug) {
-                        points += bugs[oBug]["c"];
-                    }
+    var bugKeys = Object.keys(bugs);
+    for (let kb = 0; kb < bugKeys.length; kb += 1) {
+        let bug = bugKeys[kb];
+        let points = 0;
+        for (var kob = 0; kob < bugKeys.length; kob += 1) {
+            var oBug = bugKeys[kob];
+            if (oBug in powers[bug]["s"]) {
+                points -= bugs[oBug]["c"];
+            } else {
+                if (oBug in powers[bug]["w"] || oBug === bug) {
+                    points += bugs[oBug]["c"];
                 }
             }
-            bugs[bug]["points"] = points;
         }
-    } catch {
-        console.log(bugKeys);
+        bugs[bug]["points"] = points;
     }
     if (center === -1) {
         for (let kb = 0; kb < bugKeys.length; kb += 1) {
@@ -248,9 +272,11 @@ function judge(cell, powers, rules) {
     }
     if (winners.length == 0) return -1;
     if (winners.length == 1) return winners[0];
-    var fwinner = winners[0];
+    var fwinner;
     for (let i = 0; i < winners.length; i++) {
-        if (bugs[winners[i]]["points"] == maxPoints) fwinner = winners[i];
+        if (bugs[winners[i]]["points"] == maxPoints) {
+            fwinner = fwinner ? -1 : winners[i];
+        }
     }
     return fwinner;
 }
@@ -259,23 +285,23 @@ function setRules(rules) {
     rules["d"] = {
         type: "or",
         mns: 0,
-        mxs: 1,
+        mxs: 2,
         mnp: -10,
-        mxp: 3,
+        mxp: 1,
     };
     rules["l"] = {
         type: "and",
         mns: 3,
-        mxs: 7,
-        mnp: 5,
-        mxp: 10,
+        mxs: 5,
+        mnp: 2,
+        mxp: 5,
     };
     rules["b"] = {
         type: "and",
         mns: 2,
-        mxs: 3,
-        mnp: 2,
-        mxp: 10,
+        mxs: 4,
+        mnp: 3,
+        mxp: 20,
     };
 }
 
